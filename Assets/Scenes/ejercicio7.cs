c67ccc05b92dfa2ad5520101fc45a9599b64c59c
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2022-03-31 15:53:21 -0300

    7 y 8

---
+++ Assets/Scenes/ejercicio7.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio7 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int num3;
+    // Realizá un programa que al ingresar por Inspector tres números enteros num1, num2 y
+    // num3, muestre el valor del mayor de todos.
+    // En caso de igualdad entre los tres imprimir &quot;Los números son iguales&quot;. Test1: num1 =
+    // 12, num2 = 4, num3= 7. Resultado esperado: El mayor número es num1 Test2: num1
+    // = 2, num2 = 65, num3= 8. Resultado esperado: El mayor número es num2
+    void Start()
+    {
+        if (num1 == num2 && num2 == num3)
+        {
+            Debug.Log("Los tres numeros son iguales: " + num1);
+            return;
+        }
+
+        int[] numeros = new int[3];
+        int mayornum = 0;
+        int buscador = 0;
+        int encontrador = 0;
+
+
+        numeros[0] = num1;
+        numeros[1] = num2;
+        numeros[2] = num3;
+
+        foreach(int i in numeros)
+        {
+            if (i > mayornum)
+            {
+                mayornum = i;
+                encontrador = buscador;
+            }
+            buscador++;
+        }
+
+
+        Debug.Log("El numero mas grande es: " + numeros[encontrador]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+//El foreach evalua todos los indices de un array, empezando desde 0. El valor que contiene el indice evaluado,
+//Mientras no sea nulo, va a ser asignado a la variable incializada en el foreach (en este caso la variable i)
+//Este algoritmo en particular tiene 2 partes principales: Buscar el valor maximo en el array, y guardar su indice.
+//mayornum es la variable que almacena el valor si es encontrado. 
+// Al hacer if(i > mayornum), si encuentra un valor mayor al numero mayor previamente encontrado, actualiza mayornum.
+// encontrador y buscador cumplen la segunda parte del algoritmo, que es almacenar el indice del valor maximo. 
+// Como el foreach pasa por todos los valores del array, si ponemos una variable que incrementa por 1 cada vez que 
+// Se completa el array, podemos saber cual es el indice siendo evaluado.
+// Enonces, cuando se encuentra un nuevo valor maximo, encontrador se actualiza al valor del indice del numero mas grande
+// Encontrado
---
+++ Assets/Scenes/ejercicio8.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio8 : MonoBehaviour
+{
+    // Crear una variable entera que pueda ser modificada desde el Inspector. Suponiendo que
+    // el primer día de la semana es el Domingo, mostrar el nombre del día correspondiente al
+    // valor de la variable “dia”. Si el día no está en el rango permitido debe emitir por pantalla el
+    // mensaje de error "El día ingresado no es válido".
+
+    public int dia;
+    void Start()
+    {
+        switch (dia)
+        {
+            case 1:
+                Debug.Log("El dia es Domingo");
+                break;
+            case 2:
+                Debug.Log("El dia es Lunes");
+                break;
+            case 3:
+                Debug.Log("El dia es Martes");
+                break;
+            case 4:
+                Debug.Log("El dia es Miercoles");
+                break;
+            case 5:
+                Debug.Log("El dia es Jueves");
+                break;
+            case 6:
+                Debug.Log("El dia es Viernes");
+                break;
+            case 7:
+                Debug.Log("El dia es Sabado");
+                break;
+            default:
+                Debug.Log("El dia ingresado no es valido");
+                break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
