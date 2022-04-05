c1a2baa6f56191df2b694bbf4ad768f956df9dcd
AgusFeld <101400409+AgusFeld@users.noreply.github.com>
2022-04-05 14:37:39 -0300

    9 y 10

---
+++ Assets/Scenes/ejercicio10.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//10. Realizá un programa que resuelva el siguiente problema: Nos piden desarrollar un
+//programa que informe si un estacionamiento se encuentra o no abierto según la hora
+//ingresada por el usuario.
+//Nosotros sabemos que el estacionamiento se encuentra abierto entre las 10 y las 18hs.
+//(Crear constante HORA_APERTURA = 10, y HORA_CIERRE = 18).
+//Al iniciar el programa, se tomará como hora actual el valor de una variable expuesta en el
+//Inspector, y deberemos setear una variable booleana llamada estaAbierto en true o false,
+//según corresponda, e informar por pantalla mediante la misma si el estacionamiento se
+//encuentra abierto o no.Obs: En el caso de que el valor de la variable correspona a una hora
+//menor a 0 o mayor a 24, mostrar el mensaje: “Ha ingresado una hora incorrecta”
+
+public class ejercicio10 : MonoBehaviour
+{
+    public int hora_actual;
+    int HORA_APERTURA = 10;
+    int HORA_CIERRE = 18;
+    bool estaAbierto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(hora_actual >= 0 && hora_actual <= 24)
+        {
+            if (hora_actual > HORA_APERTURA && hora_actual < HORA_CIERRE)
+            {
+                estaAbierto = true;
+            }
+            else
+            {
+                estaAbierto = false;
+            }
+            if (estaAbierto == true)
+            {
+                Debug.Log("El Establecimiento esta Abierto.");
+            }
+            else if (estaAbierto == false)
+            {
+                Debug.Log("El establecimiento esta Cerrado.");
+            }
+        }
+        else if (hora_actual <= 0 || hora_actual >= 24)
+        {
+            Debug.Log("Ha ingresado una hora incorrecta.");
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scenes/ejercicio9.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//9. Realizá un programa que resuelva el siguiente problema:
+//tres personas aportan diferente capital a una sociedad.Se desea saber qué porcentaje del
+//total aportó cada una (indicando nombre y porcentaje) y cuál es el monto del total aportado
+//por las tres.
+//Ingresar por Inspector el ingreso del capital aportado por cada una de las personas y luego
+//mostrar lo pedido en el siguiente formato:
+//Nombre: capital aportado: $ … , Porcentaje del capital: %.... , Monto total aportado: $ ….
+
+public class ejercicio9 : MonoBehaviour
+{
+    public float capital1;
+    public float capital2;
+    public float capital3;
+
+    public string nombre1;
+    public string nombre2;
+    public string nombre3;
+
+    float porcentaje1;
+    float porcentaje2;
+    float porcentaje3;
+    float monto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        monto = capital1 + capital2 + capital3;
+        porcentaje1 = capital1 / monto * 100;
+        porcentaje2 = capital2 / monto * 100;
+        porcentaje3 = capital3 / monto * 100;
+
+        Debug.Log("Nombre: " + nombre1 + ", capital aportado: $" + capital1 + "," +
+            " Porcentaje del capital: %" + porcentaje1 + ", Monto total aportado: $" + monto + ".");
+        Debug.Log("Nombre: " + nombre2 + ", capital aportado: $" + capital2 + "," +
+            " Porcentaje del capital: %" + porcentaje2 + ", Monto total aportado: $" + monto + ".");
+        Debug.Log("Nombre: " + nombre3 + ", capital aportado: $" + capital3 + "," +
+            " Porcentaje del capital: %" + porcentaje3 + ", Monto total aportado: $" + monto + ".");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
